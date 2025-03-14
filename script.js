ce78913716572d5bf00acf749f2c14ac0aa66791
saripudin14 <m.arifrivaldi141105@gmail.com>
2025-03-14 13:12:23 +0700

    add header

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed("#typing", {
+        strings: ["Informatics Engineering Student", "Junior Web Developer", "IT Implementor"],
+        typeSpeed: 100,  // Kecepatan mengetik
+        backSpeed: 50,   // Kecepatan menghapus
+        backDelay: 1500, // Jeda sebelum ganti teks
+        loop: true       // Loop biar terus berjalan
+    });
+});
